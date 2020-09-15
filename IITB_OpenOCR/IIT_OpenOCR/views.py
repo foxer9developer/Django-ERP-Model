97508fd922881f223e504d32b160efcd034001ad
Sanoj Senthilvelavan <svtsanoj@gmail.com>
2020-09-15 10:01:55 +0530

    requirements.txt added, navbar modified

--- IITB_OpenOCR/IIT_OpenOCR/views.py
+++ IITB_OpenOCR/IIT_OpenOCR/views.py
@@ -2,28 +2,25 @@ from django.shortcuts import render, redirect
 from .models import users, SetStatus
 from django.contrib import messages
 from django.contrib.auth import logout
+from github import Github
+
 
 def home(request):
     if request.user.is_authenticated:
         count = users.objects.filter(github_username = request.user.username).count()
+        social = request.user.social_auth.get(provider='github')
+        access_token = social.extra_data['access_token']
+        g = Github(access_token)
+        g.get_repos
+        repo = g.get_repo("svtsanoj/Open-OCR-Correct")
+        contents = repo.get_contents("README.md")
+        print(contents)
         if(count != 1):
             logout(request)
             messages.info(request, 'This account is not registered to Akshar Anveshini. Please Register with to Continue')
             return redirect('/register')
     else:
         return redirect('/login')
-
-    # usersObjects = users.objects.all()
-    # setStatusObjects = SetStatus.objects.all()
-    # for user in usersObjects:
-    #     if(user['user_role']!="Project Manager"):
-    #         for i, setstatus in enumerate(setStatusObjects):
-    #             if (setstatus['github_username'] == user['github_username']):
-    #                 usersObjects[user['sets_completed']] = setstatus['sets_completed']
-    #                 usersObjects[user['pages_completed']] = setstatus['pages_completed']
-    #                 usersObjects[user['avg_rating']] = setstatus['avg_rating']
-    #                 del setStatusObjects[i]
-    #                 break
     context = {
         'users': users.objects.all()
             }
